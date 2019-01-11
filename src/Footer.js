6be2c97d1e6956cd3fe45bd539b0774209046b7f
sarahcalande <scalande@yahoo.com>
2019-01-11 17:56:23 -0500

    content no styling

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component{
+  render(){
+    return(
+      <div classname="Header">
+      <a href= "mailto: sarahcalande@gmail.com"> Contact</a>|
+      <a href='www.github.com/sarahcalande'> Github</a>|
+      <a href='www.linkedin.com/in/sarahcalande'> Linkedin</a>|
+      <a href='https://www.youtube.com/channel/UC3KOQiAXxauobybW52SgTNw'>YouTube Demos</a>|
+      <a href='www.medium/@sarahcalande'>Technical Blog</a>
+      </div>
+    )
+  }
+}
---
+++ src/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component{
+  render(){
+    return(
+      <div classname="Header">
+      <h1>Sarah Calande</h1>
+      </div>
+    )
+  }
+}
