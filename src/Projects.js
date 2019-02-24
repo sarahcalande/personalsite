45a1aae10569433c8bc883ff2ebad8cf4a5e42b5
sarahcalande <scalande@yahoo.com>
2019-02-24 09:28:28 -0500

    update

--- src/Projects.js
+++ src/Projects.js
@@ -2,19 +2,38 @@ import React, { Component } from 'react';
 
 
 
-handleHover(e){
-  if
+export default class Projects extends Component{
+
+state = {
+  clicked: false
 }
 
-export default class Projects extends Component{
+
+  handleHover(){
+    this.setState({
+      clicked: true
+    })
+  }
+
+
+
+
   render(){
+    if (this.state.clicked === true){
+      return (
+        <div>
+        <iframe width="450" height="350" src="https://www.youtube.com/embed/8H_S9hPYpXA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="450" height="350" src="https://www.youtube.com/embed/lRnOAxW6lCU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="450" height="350" src="https://www.youtube.com/embed/Ks10yM6HF54" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="450" height="350" src="https://www.youtube.com/embed/WJy3Uix1sJM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      )
+    } else {
     return(
-      <div onHover = {this.handleHover()}>
-      <iframe width="450" height="350" src="https://www.youtube.com/embed/8H_S9hPYpXA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe width="450" height="350" src="https://www.youtube.com/embed/lRnOAxW6lCU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe width="450" height="350" src="https://www.youtube.com/embed/Ks10yM6HF54" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe width="450" height="350" src="https://www.youtube.com/embed/WJy3Uix1sJM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <div onClick = {this.handleHover()}>
+      <h3>Projects</h3>
       </div>
     )
   }
+}
 }
