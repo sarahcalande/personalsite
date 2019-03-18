8ede759b3e5bc5b57829c3db9ba3b286b2ee4350
sarahcalande <scalande@yahoo.com>
2019-03-17 23:12:48 -0400

    change

--- src/Body.js
+++ src/Body.js
@@ -22,14 +22,14 @@ export default class Body extends Component{
       I was born in Philadelphia, raised in Maine, and studied at the University of Southern California in Los Angeles. These changes in environment and diversity made me eager to learn and share as much as I could. As a karate teacher since 2010 and english teacher since 2012, I always knew that knowledge led to opportunity. I started karate in 2004 and trained nearly every day for hours a day, until I became a 3x North American Sport Karate Association World Champion. This procurement showed me from an early age that anything can be accomplished with enough hard work, dedication, and focus. I studied abroad in Paris, graduated college, and came back to Maine to work for a year while I continued taking courses to explore my interests in Nutrition and Microeconomics. As I enjoyed working as the manager of a refugee mental health center with many clients from French speaking countries, I decided to go back to France for full language immersion. While in France, teaching English, learning in French, and communicating in Chinese with my online students, I shifted my aptitude for lingustics into a hard skill as I started studying coding languages. In the past year I have gone from 'Hello World' to building websites and applications for clients. Coding is the way that I can remain a student for life while giving myself and others a platform to share our unique passions.
 
 
-      <h3>Education 🎓</h3>
+      <h3>Education</h3>
       University of Southern California Bachelor's Degree 2012 – 2016 <br></br>
       Flatiron School Software Engineering Immsersive 2018 <br></br>
       IS Aix-en-Provence Certificat Niveau Francais C1 2017<br></br>
       Southern Maine Community College Nutrition and Microeconomics courses for continued education, 4.0 overall 2017<br></br>
       Université Paris-Sorbonne Certificat Grammaire française 2016
 
-<h3>Technical Skills 📊</h3>
+<h3>Technical Skills</h3>
 LANGUAGES: JavaScript ES6+, Ruby, HTML, CSS, Python, TypeScript <br></br>
 FRAMEWORKS/LIBRARIES: React Native, React.js, Redux + Thunk, Node.js, Rails, jQuery, Mocha Test Driven Development, Chai Expect Behavioral Driven Development <br></br>
 DATABASES: PostGreSQL, SQLite <br></br>
